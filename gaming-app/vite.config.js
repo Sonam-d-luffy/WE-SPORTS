34a5d8da937d92d1e4f815fbee9595452ef65554
Sonam Kumari <sonam992007@gmail.com>
2025-09-18 19:45:56 +0530

    changes

--- gaming-app/vite.config.js
+++ gaming-app/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Anything that starts with /pincode will go to Postal API
+      "/pincode": {
+        target: "https://api.postalpincode.in",
+        changeOrigin: true,
+        secure: true, // since it's HTTPS
+        rewrite: (path) => path.replace(/^\/pincode/, "")
+      }
+    }
+  }
 })
